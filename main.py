d00259a7f81b7c4be551f8025f37ed2ecc38ba3c
ea-oconnor <oconnor.eamon0@gmail.com>
2024-12-26 20:53:28 -0500

    Add qqplot, mann-whitney u test

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from modules import utils, query
 import argparse
 import pandas as pd
 import scipy.stats as stats
+import statsmodels.api as sm 
+import pylab as py 
 
 
-def test_pheno_genus(mesh_id, mesh_label, tax_id, tax_label, transformation, hist_dir):
+def test_pheno_genus(mesh_id, mesh_label, tax_id, tax_label, transformation, out_dir):
     """
     Pull data from GMrepo for given mesh/tax, print stats, make hist
     @param mesh_id: NIH MeSH ID of phenotype
@@ -16,7 +18,7 @@ def test_pheno_genus(mesh_id, mesh_label, tax_id, tax_label, transformation, his
     @param tax_id: NIH taxonomy ID of species/genus
     @param tax_label: Scientific name of species/genus
     @param transformation: Type of transformation to apply to data. Expects 'log10', 'ln', 'boxcox'
-    @param hist_dir: directory to write histogram to
+    @param out_dir: directory to write plots to
     @return None
     """
     # Retrieve data
@@ -26,33 +28,31 @@ def test_pheno_genus(mesh_id, mesh_label, tax_id, tax_label, transformation, his
     disease_norm = utils.transform(disease_data, transformation)
     health_norm = utils.transform(health_data, transformation)
 
-    # Check Normality
-    #p_norm_disease = stats.shapiro(disease_norm).pvalue
-    #p_norm_health = stats.shapiro(health_norm).pvalue
+    # QQ Plots to check Normality
+    sm.qqplot(health_norm, line ='45')
+    py.savefig(out_dir+'/qq_health.png', dpi=300)
+    py.clf()
+    sm.qqplot(disease_norm, line ='45')
+    py.savefig(out_dir+'/qq_pheno.png', dpi=300)
 
     # Calculate means and standard deviations
     disease_mean, disease_stdev = utils.basic_stats(disease_data)
     health_mean, health_stdev = utils.basic_stats(health_data)
 
-    # Perform Welch t-test if transformed data is normal
-    #if p_norm_disease > 0.05 and p_norm_health > 0.05:
-    p_welch = stats.ttest_ind(disease_norm, health_norm, equal_var = False, alternative='less').pvalue
-    #    norm = True
-    #else:
-    #    norm = False
-
-    norm=True
+    # Perform Welch t-test
+    p_welch = stats.ttest_ind(disease_norm, health_norm, equal_var = False, alternative='greater').pvalue
+    
+    # Mann Whitney U Test
+    p_mw = stats.mannwhitneyu(disease_data, health_data).pvalue
 
     # Generate histogram
-    utils.hist(disease_norm, health_norm, mesh_label, tax_label, hist_dir)
+    utils.hist(disease_norm, health_norm, mesh_label, tax_label, out_dir)
 
     # Print stats
     print(f'The mean relative abundance of {tax_label} in the {mesh_label} group is {round(disease_mean,3)}, with a standard deviation of {round(disease_stdev,3)}.')
     print(f'The mean relative abundance of {tax_label} in the health group is {round(health_mean,3)}, with a standard deviation of {round(health_stdev,3)}.')
-    if norm == True:
-        print(f'A Welch\'s t-test of {tax_label} abundance between the {mesh_label} group and healthy group produced a p-value of {p_welch:.3e}.')
-    elif norm == False:
-        print(f'A Shapiro-Wilk test for normality yielded a p-value of {p_norm_disease} for the {mesh_label} group and {p_norm_health} for the health group. At least one dataset is not normal and a Welch\'s t-test cannot be performed.')
+    print(f'A Welch\'s t-test of {tax_label} abundance between the {mesh_label} group and healthy group produced a p-value of {p_welch:.3e}.')
+    print(f'A Mann-Whitney U test of {tax_label} abundance between the {mesh_label} group and healthy group produced a p-value of {p_mw:.3e}.')
 
 
 def get_fh_argparse():
