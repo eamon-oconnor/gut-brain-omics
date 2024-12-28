7dcc76f503c806b648dd61d7fa3bc18efc032ad3
ea-oconnor <oconnor.eamon0@gmail.com>
2024-12-28 15:38:42 -0500

    Remove unnecessary files

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from modules import query
 import main
 
-mesh_label = 'Parkinson Disease'
+mesh_label = 'Depression'
 mesh_id = query.retrieve_mesh_id(mesh_label)
 #mesh_id = 'D013313'
 #mesh_label = query.retrieve_mesh_descriptor(mesh_id)
 
-tax_id, tax_label = query.retrieve_tax_info('Enterococcus')
+tax_id, tax_label = query.retrieve_tax_info('Bifidobacterium')
 
 main.test_pheno_genus(mesh_id, mesh_label,
                       tax_id, tax_label,
